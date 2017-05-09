2b38b8fe82d4ad0b25dc143a600551b9fde892d7
alexandrawaite <alexandrawaite@gmail.com>
2017-05-09 15:48:32 -0700

    stage 2 specifications

--- bike-shop/src/stage2-constructors.js
+++ bike-shop/src/stage2-constructors.js
@@ -1,14 +1,29 @@
-function Bike() {
-  // your code here
+function Bike(name, price, rings = [3,7] ) {
+  this.name = name;
+  this.price = price;
+  this.tires = [new Tire(), new Tire()];
+  this.frame = new Frame();
+  this.rings = rings;
+  this.brakes = new Brakes();
 }
 
-function Frame() {
-  // your code here
+function Frame(color = 'black', size = 55, style = 'street') {
+  this.color = color;
+  this.size = size;
+  this.style = style;
 }
 
-function Tire() {
-  // your code here
+function Tire(diameter = 22, type = 'street') {
+  this.diameter = diameter;
+  this.type = type;
 }
+
+function Brakes(front = true, back = true) {
+  this.front = front;
+  this.back = back;
+}
+
+
 
 module.exports = {
   Bike: Bike,
---
+++ jsinfo/create-new-calculator.js
@@ -0,0 +1,19 @@
+function Calculator() {
+  this.read = function() {
+    this.first = +prompt('First number: ', 0);
+    this.second = +prompt('Second number: ', 0);
+  };
+  this.sum = function() {
+    return this.first + this.second;
+  };
+  this.mul = function() {
+    return this.first * this.second;
+  };
+}
+
+
+let calculator = new Calculator() {
+  calculator.read();
+  alert("Sum = " + calculator.sum());
+  alert("Mul = " + calculator.mul());
+}
